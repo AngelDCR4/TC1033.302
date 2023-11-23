4ba814b171bf2e39cc78f2cabcad2f4ff985184c
AngelDCR4 <149647380+AngelDCR4@users.noreply.github.com>
2023-11-23 02:21:09 -0600

    Update papeleria.h

--- papeleria.h
+++ papeleria.h
@@ -1,9 +1,11 @@
-#include <string>
 #ifndef Papeleria_H
 #define Papeleria_H
 #include "empleados.h"
 #include "materiales.h"
 #include "servicios.h"
+#include <string>
+#include <iostream>
+#include <sstream>
 /*
 #include "Impresora"
 #include "Vender Material"
@@ -13,6 +15,7 @@
 class Papeleria{
     private:
         std::string nombre_pape;
+        Materiales material[4];
     public:
         Papeleria(): nombre_pape(""){};
         Papeleria(std::string nom):nombre_pape(nom){};
@@ -23,6 +26,8 @@ class Papeleria{
 //FUNCION EXTRA
         void despedir(int);
         void contratar(int);
+        void agregarMaterial(std::string, int, int);
+        std::string anaqueles();
 };
     std::string Papeleria::get_nombre_pape(){
         return nombre_pape;
@@ -37,6 +42,17 @@ class Papeleria{
     }
     void Papeleria::contratar(int Con){
         
+    }
+    void Papeleria::agregarMaterial(std::string nombre_material, int cantidad, int no_anaquel){
+        material[no_anaquel] = Materiales(nombre_material,cantidad);
+    }
+
+    std::string Papeleria::anaqueles(){
+        std::stringstream aux;
+            for(int i = 0; i < 4; i++){
+                aux << "Material: " << i << " " << this->material[i].to_string() << "\n";
+            }
+            return aux.str();
     }
 
 #endif
