02578587e751db6abdd1ad6df356d36a4deb6600
AngelDCR4 <149647380+AngelDCR4@users.noreply.github.com>
2023-11-30 02:55:32 -0600

    Update mainpape.cpp

--- mainpape.cpp
+++ mainpape.cpp
@@ -1,36 +1,61 @@
+/*
+ * Proyecto Papeleria
+ * Angel David Candelario Rolon
+ * A01712387
+ * 30/11/2023
+ */
+
+/*
+ * Descripción: este proyecto para la clase de Pensamiento
+ * Computacional Orientado a objetos. Es un programa que almacena
+ * los productos principales de una papeleria guardando principalmente
+ * su precio y su cantidad de los tipos de kits de papeleria
+ */
 #include <iostream>
 #include <string>
 #include "papeleria.h"
-#include "empleados.h"
 #include "materiales.h"
-#include "servicios.h"
-#include "impresora.h"
-#include "vendermaterial.h"
-#include "clientes.h"
 
 using namespace std;
 
 int main(){
+    bool cont = false;
+    int seg;
+    Papeleria pape_1;
+    string nom;
+    int res;
+    int cant_of;
+    float pre_of;
+    int cant_es;
+    float pre_es;
 
-    Papeleria pape_1("Papeleria la Lupita");
-    Empleados emp_1("Juanito",24,4241.50);
-    Servicios serv_1(3,10);
-    Impresora imp_1(2.5,2);
-    Vendermaterial vend_1(10);
-    Clientes cli_1("Omar",250);
-    cout << pape_1.get_nombre_pape() << endl;
-    cout << emp_1.get_nombre() << " " << emp_1.get_edad() << " " << emp_1.get_salario() << endl;
-    cout << serv_1.get_precio_papel() << " " << serv_1.get_precio_mat_esc() << endl;
-    cout << imp_1.get_costo_impresion() << " " << imp_1.get_costo_escaneo() << endl;
-    cout << vend_1.get_costo_materiales() << endl;
-    cout << cli_1.get_nombre() << " " << cli_1.get_dinero() << endl;
+    cout << "Nombre de la papeleria: ";
+    getline(cin,nom);
+    pape_1.set_nombre_pape(nom);
+
+    cout << "--------------> Papeleria "<< pape_1.get_nombre_pape() << " <-------------- "<< endl;
+    cout << "Las ganancias actuales son: "<< pape_1.get_ganancias() << endl;
+
+    cout << "Su almacen de kits esta vacio" << endl;
+    cout << "Defina la cantidad de kits oficinista a anadir: ";
+    cin >> cant_of;
+    cout << "Defina su precio: ";
+    cin >> pre_of;
+
+    cout << "Defina la cantidad de kits escolares: ";
+    cin >> cant_es;
+    cout << "Defina su precio: ";
+    cin >> pre_es;
+    cout << "-----------------------------------------------------------------------------" << endl;
+    pape_1.agrega_office("Pro-ofice","Kit oficinista",pre_of);
+    pape_1.agrega_school("School-kid", "Kit escolar", pre_es);
+
+    pape_1.inventario();
+
+    cout << "Desea vender productos de:" << endl;
+    cout << "Oficina  ---> 1" << endl << "Escuela ---> 2" << endl << "Todos ---> 3" << endl << "Seleccione una opcion: ";
+    cin >> res;
+    pape_1.vender(res,pre_of,pre_es, cant_of,cant_es);
+    cout << "Las ganancias actuales son: "<< pape_1.get_ganancias() << endl;
     
-    //AGREGACION
-    pape_1.agregarMaterial("Lapicez",10,0);
-    pape_1.agregarMaterial("Gomas",12,1);
-    pape_1.agregarMaterial("Hojas",50,2);
-    pape_1.agregarMaterial("Plumas",0,3);
-
-    cout << pape_1.anaqueles();
-
 }
