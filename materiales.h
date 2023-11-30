1dbc09214316d55090b7821905519002852d41d8
AngelDCR4 <149647380+AngelDCR4@users.noreply.github.com>
2023-11-30 02:56:27 -0600

    Update materiales.h

--- materiales.h
+++ materiales.h
@@ -1,27 +1,39 @@
-#include <string>
+/*
+ * Proyecto Papeleria
+ * Angel David Candelario Rolon
+ * A01712387
+ * 30/11/2023
+ */
+
+/*
+ * Clase Materiales, tiene como atributo la marca del material
+ * y lo hereda para los demas tipos de kits
+ */
+
 #ifndef Materiales_H
 #define Materiales_H
+#include <string>
 #include <sstream>
 
 class Materiales{
     private:
-        std::string nombre_material;
-        int cantidad;
+        std::string marca_material;
     public:
-        Materiales(){};
-        Materiales(std::string nom, int cant):nombre_material(nom),cantidad(cant){};
-        std::string to_string();
+        Materiales():marca_material(""){};
+        Materiales(std::string mar):marca_material(mar){};
 //GETTER
-
+        std::string get_marca();
 //SETTER
+        void set_marca(std::string mar);
 
-//FUNCION EXTRA
 };
 
-std::string Materiales::to_string(){
-    std::stringstream aux;
-    aux << nombre_material << " " << "son " << cantidad;
-    return aux.str();
+std::string Materiales::get_marca(){
+        return marca_material;
 }
+
+void Materiales::set_marca(std::string mar){
+        marca_material = mar;
+}       
 
 #endif
